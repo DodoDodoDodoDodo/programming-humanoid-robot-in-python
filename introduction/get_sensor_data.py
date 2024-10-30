b0accae8bb054b9aa4657d76f1c857073573c3d7
Philipp Zorn <zorn@campus.tu-berlin.de>
2024-10-30 14:19:27 +0100

    desparate attempt #2

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -1,16 +1,21 @@
-'''
+"""
 In this exercise you need to know how to get sensor data.
 
 * Task: get the current joint angle and temperature of joint HeadYaw
 
 * Hint: The current sensor data of robot is stored in perception (class Perception in spark_agent.py)
 
-'''
+"""
 
 # add PYTHONPATH
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
+
+sys.path.append(
+    os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "..", "software_installation"
+    )
+)
 
 from spark_agent import SparkAgent
 
@@ -21,10 +26,21 @@ class MyAgent(SparkAgent):
         temperature = 0
         # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
+        try:
+            angle = perception.joint["HeadYaw"] if "HeadYaw" in perception.joint else 0
+            temperature = (
+                perception.joint_temperature["HeadYaw"]
+                if "HeadYaw" in perception.joint_temperature
+                else 0
+            )
+        except:
+            angle, temperature = 0, 0
+        # get angle and temperature to current data of joint HeadYaw
 
-        print('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
+        print("HeadYaw angle: " + str(angle) + " temperature: " + str(temperature))
         return super(MyAgent, self).think(perception)
 
-if '__main__' == __name__:
+
+if "__main__" == __name__:
     agent = MyAgent()
     agent.run()
--- introduction/set_joint_commands.py
+++ introduction/set_joint_commands.py
@@ -1,4 +1,4 @@
-'''
+"""
 In this exercise you need to know how to set joint commands.
 
 * Tasks:
@@ -7,12 +7,17 @@ In this exercise you need to know how to set joint commands.
 
 * Hint: The commands are stored in action (class Action in spark_agent.py)
 
-'''
+"""
 
 # add PYTHONPATH
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'software_installation'))
+
+sys.path.append(
+    os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "..", "software_installation"
+    )
+)
 
 from spark_agent import SparkAgent
 
@@ -21,9 +26,18 @@ class MyAgent(SparkAgent):
     def think(self, perception):
         action = super(MyAgent, self).think(perception)
         # YOUR CODE HERE
-
+        stiffness = 0
+        HeadYaw = 0.1
+        try:
+            action.stiffness["LShoulderPitch"] = max(
+                0, min(1, stiffness)
+            )  # Clamp between 0 and 1
+            action.speed["HeadYaw"] = max(-1, min(1, HeadYaw))  # Clamp between -1 and 1
+        except:
+            pass  # Maintain default values if assignment fails
         return action
 
-if '__main__' == __name__:
+
+if "__main__" == __name__:
     agent = MyAgent()
     agent.run()
